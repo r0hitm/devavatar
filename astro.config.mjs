a6b093134f6bc1eac93c4e8ecde3b7b81d946ee4
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-13 11:49:11 -0700

    update to devavatar.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://devavatar.com',
     integrations: [mdx(), sitemap()],
     vite: {
         plugins: [tailwindcss()]
