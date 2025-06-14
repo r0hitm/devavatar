faf242357c516d4445b9e0c15a858d591ef022dc
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-14 11:14:22 -0700

    install react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://devavatar.com",
     prefetch: true,
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), react()],
     vite: { plugins: [tailwindcss()] }
 });
