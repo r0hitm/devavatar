2bdb58b916fe446363a09702b7baf03c3c5150e6
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-14 11:34:02 -0700

    config markdown shiki and toc plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,46 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://devavatar.com",
     prefetch: true,
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [
+        mdx(),
+        sitemap({
+            serialize(sitemap_item) {
+                // THIS IS FROM LEGACY DevAvatar
+                const url = sitemap_item.url;
+                const isRoot = /devavatar.com\/$/.test(url);
+                const isAbout = /about\/$/.test(url);
+                const isCredits = /credits\/$/.test(url);
+                const postSlug =
+                    url.match(/\/posts\/([^/]+)\/?$/)?.[1] ?? false;
+                const isPost = postSlug && isNaN(Number(postSlug));
+
+                if (isRoot || isAbout || isCredits || isPost) {
+                    if (isPost) {
+                        sitemap_item.priority = 1;
+                    }
+                    return sitemap_item;
+                } else {
+                    return undefined;
+                }
+            },
+            changefreq: "weekly",
+            lastmod: new Date(),
+            priority: 0.8
+        }),
+        react()
+    ],
+    markdown: {
+        shikiConfig: {
+            theme: "one-dark-pro",
+            wrap: true
+        },
+        remarkPlugins: [[remarkToc, { heading: "Table of contents" }]]
+    },
     vite: { plugins: [tailwindcss()] }
 });
--- src/content/blog/test-post.md
+++ src/content/blog/test-post.md
@@ -10,8 +10,23 @@ Intro
 
 ## Table of contents
 
+## heading 1
+
 Nani sore? Dattebayo! Kira kira, kawaii desu ne. Sugoi, senpai notice me! Arigato gozaimasu. Itadakimasu, baka gaijin. Chibi neko, kawaii. Moshi moshi, onii-chan.
+
+## headng 2
 
 Yamete kudasai! Isekai, shonen jump, mahou shoujo. Sugoi desu ne. Waku waku, doki doki. Moe moe kyun! Yare yare daze. Omae wa mou shindeiru. Nani?!
 
+## heading 3
+
 Kawaii desu ka? Baka! Tsundere, yandere, kuudere. Senpai, daisuki! Chotto matte. Sumimasen. Gomen nasai. Ikimashou! Tatakae!
+
+### subheading 1
+
+testkljsa ksjdgkajsdk
+
+```js
+console.log('test')
+```
+code^
