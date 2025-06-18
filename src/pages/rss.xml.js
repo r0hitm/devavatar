b6d4560d34eaa17144e727e3d31048ebad9fa00d
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-18 01:11:16 -0700

    add post index card and a getPosts function wrapper around getColections (to handle drafts)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,9 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
 import { SITE } from "@/consts";
+import getPosts from "@/utils/getPosts";
 
 export async function GET(context) {
-    const posts = await getCollection("blog");
+    const posts = await getPosts();
     return rss({
         title: SITE.title,
         description: SITE.description,
---
+++ src/utils/getPosts.ts
@@ -0,0 +1,10 @@
+import { getCollection, type CollectionEntry } from "astro:content";
+
+export default async function getPosts(): Promise<CollectionEntry<"blog">[]> {
+    const isDev = import.meta.env.DEV;
+    const posts = isDev
+        ? await getCollection("blog")
+        : await getCollection("blog", ({ data }) => !data.draft);
+
+    return posts;
+}
