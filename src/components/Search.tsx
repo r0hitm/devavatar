00a6303f34dcb375af6d8e37cd42d45c75e0c938
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-21 11:58:49 -0700

    search padding

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -62,7 +62,7 @@ export default function Search({ searchList }: Props) {
 
     return (
         <>
-            <button className="group" onClick={() => setIsOpen(true)}>
+            <button className="group p-2" onClick={() => setIsOpen(true)}>
                 <SearchIcon className="text-d-txt-base group-hover:text-d-accent size-4" />
                 <span className="sr-only">Search</span>
             </button>
