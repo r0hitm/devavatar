9fa45288f976949a2c142bf6b537fcca67e75ddc
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-09-21 20:27:27 +0900

    todo for later

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,8 @@
 import { en } from "./en";
 import { ja } from "./ja";
 
+// TODO: Optimize it later, instead of returning everything
+// a curried function to return the translation directly maybe
 export function useTranslations(lang?: string) {
     if (lang === "ja") {
         return ja;
