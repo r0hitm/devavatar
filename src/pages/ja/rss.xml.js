6dbee758132a7da0ceb60b7dbe2c0cde24716a6c
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-09-28 16:27:47 +0900

    localized rss

---
+++ src/pages/ja/rss.xml.js
@@ -0,0 +1,16 @@
+import rss from "@astrojs/rss";
+import { SITE } from "@/consts";
+import getPosts from "@/utils/getPosts";
+
+export async function GET(context) {
+    const posts = await getPosts();
+    return rss({
+        title: SITE.title,
+        description: SITE.description,
+        site: context.site,
+        items: posts.map(post => ({
+            ...post.data,
+            link: `/ja/posts/${post.id}/`
+        }))
+    });
+}
